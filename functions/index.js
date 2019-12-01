906cd01ae2f1520196eb79ade7255cd362fbee51
Kai Sawamoto <kaisawamoto@gmail.com>
2019-12-01 09:58:08 -0800

    insert works! but firebase serve dont support

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+
 admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -22,4 +23,26 @@ exports.getScreams = functions.https.onRequest((req, res) => {
 			res.json(screams);
 		})
 		.catch((err) => console.error(err));
+});
+
+// create scream
+exports.createScream = functions.https.onRequest((req, res) => {
+	const newScream = {
+		body: req.body.body,
+		user_handle: req.body.user_handle,
+		created_at: admin.firestore.Timestamp.fromDate(new Date())
+	};
+	admin
+		.firestore()
+		.collection('screams')
+		.add(newScream)
+		.then((doc) => {
+			res.json({
+				message: `document ${doc.id} created successfully`
+			});
+		})
+		.catch((err) => {
+			res.status(500).json({ error: 'something went wrong' });
+			console.error(err);
+		});
 });
